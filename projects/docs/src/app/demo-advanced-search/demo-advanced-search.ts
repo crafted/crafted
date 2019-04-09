3f1e708c8342b84f624124d651d847cbc24a4f19
Andrew Seguin <andrewjs@google.com>
2019-04-09 15:29:39 -0700

    remove constructor debugging from demo advanced search

--- projects/docs/src/app/demo-advanced-search/demo-advanced-search.ts
+++ projects/docs/src/app/demo-advanced-search/demo-advanced-search.ts
@@ -18,11 +18,4 @@ export class DemoAdvancedSearch {
   filterer = new Filterer(DocsDataFiltererMetadata);
 
   exampleItems = this.dataSource.data.pipe(this.filterer.filter());
-
-  constructor() {
-    this.exampleItems.subscribe(items => {
-      console.log(items);
-      // debugger;
-    })
-  }
 }
