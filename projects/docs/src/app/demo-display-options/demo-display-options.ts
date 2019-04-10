b36b88b5154569ba3dee6b3c98e3bde9e8fc2542
Andrew Seguin <anlizbg@gmail.com>
2019-04-10 08:22:35 -0700

    move item count out of display options header

---
+++ projects/docs/src/app/demo-display-options/demo-display-options.ts
@@ -0,0 +1,21 @@
+import {Component} from '@angular/core';
+import {DataSource, Filterer, Viewer} from '@crafted/data';
+import {of} from 'rxjs';
+
+import {EXAMPLE_ITEMS, ExampleItem} from '../data';
+import {ExampleFiltererMetadata} from '../data-resources/filterer-metadata';
+import {ExampleViewerMetadata} from '../data-resources/viewer-metadata';
+
+
+@Component({
+  selector: 'demo-advanced-search',
+  templateUrl: 'demo-advanced-search.html',
+  styleUrls: ['demo-advanced-search.scss'],
+})
+export class DemoAdvancedSearch {
+  dataSource = new DataSource(new Map(), of(EXAMPLE_ITEMS));
+  viewer = new Viewer<ExampleItem>(ExampleViewerMetadata);
+  filterer = new Filterer(ExampleFiltererMetadata);
+
+  exampleItems = this.dataSource.data.pipe(this.filterer.filter());
+}
