9b203e7614ad3929cbbf0da07b9e46352f3e8e69
Andrew Seguin <andrewjs@google.com>
2019-04-16 09:10:55 -0700

    add results count to recommendation view

--- projects/github-dashboard/src/app/repository/recommendations-page/recommendation-view/recommendation-view.ts
+++ projects/github-dashboard/src/app/repository/recommendations-page/recommendation-view/recommendation-view.ts
@@ -4,12 +4,13 @@ import {ChangeDetectionStrategy, Component, Inject, Input, SimpleChanges} from '
 import {
   DataResources,
   DateQuery,
-  FiltererState,
+  Filterer,
   InputQuery,
   NumberQuery,
   StateQuery
 } from '@crafted/data';
-import {FiltererMetadata} from 'projects/data/src/public-api';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {DATA_RESOURCES_MAP} from '../../repository';
 import {ActiveStore} from '../../services/active-store';
 import {
@@ -38,7 +39,9 @@ export class RecommendationView {
 
   recommendationTypes = RecommendationTypes;
 
-  filtererString: string;
+  filtererString: Observable<string>;
+
+  resultsCount: Observable<number>;
 
   @Input() recommendation: Recommendation;
 
@@ -48,9 +51,11 @@ export class RecommendationView {
 
   ngOnChanges(simpleChanges: SimpleChanges) {
     if (simpleChanges['recommendation'] && this.recommendation) {
-      const state = this.recommendation.filtererState;
-      const metadata = this.dataResourcesMap.get(this.recommendation.data).filterer().metadata;
-      this.filtererString = getFiltererString(state, metadata);
+      const provider = this.dataResourcesMap.get(this.recommendation.data);
+      const filterer = provider.filterer(this.recommendation.filtererState);
+      this.resultsCount =
+          provider.dataSource().data.pipe(filterer.filter(), map(items => items.length));
+      this.filtererString = getFiltererString(filterer);
     }
   }
 
@@ -71,39 +76,41 @@ export class RecommendationView {
   }
 }
 
-function getFiltererString(state: FiltererState, metadata: Map<string, FiltererMetadata>): string {
-  let str = '';
+function getFiltererString(filterer: Filterer): Observable<string> {
+  return filterer.state.pipe(map(state => {
+    let str = '';
 
-  if (state.search) {
-    str += `Search: "${state.search}"`;
-  }
-
-  state.filters.forEach(filter => {
-    if (str) {
-      str += ', ';
+    if (state.search) {
+      str += `Search: "${state.search}"`;
     }
 
-    const equality = equalityToString[filter.query.equality] || filter.query.equality;
-    switch (metadata.get(filter.type).queryType) {
-      case 'input':
-        const inputQuery = filter.query as InputQuery;
-        str += `${filter.type} ${equality} "${inputQuery.input}"`;
-        break;
-      case 'number':
-        const numberQuery = filter.query as NumberQuery;
-        str += `${filter.type} ${equality} ${numberQuery.value}`;
-        break;
-      case 'date':
-        const datePipe = new DatePipe('en-us');
-        const dateQuery = filter.query as DateQuery;
-        str += `${filter.type} ${equality} ${datePipe.transform(dateQuery.date)}`;
-        break;
-      case 'state':
-        const stateQuery = filter.query as StateQuery;
-        str += `${filter.type} ${equality} ${stateQuery.state}`;
-        break;
-    }
-  });
+    state.filters.forEach(filter => {
+      if (str) {
+        str += ', ';
+      }
 
-  return str;
+      const equality = equalityToString[filter.query.equality] || filter.query.equality;
+      switch (filterer.metadata.get(filter.type).queryType) {
+        case 'input':
+          const inputQuery = filter.query as InputQuery;
+          str += `${filter.type} ${equality} "${inputQuery.input}"`;
+          break;
+        case 'number':
+          const numberQuery = filter.query as NumberQuery;
+          str += `${filter.type} ${equality} ${numberQuery.value}`;
+          break;
+        case 'date':
+          const datePipe = new DatePipe('en-us');
+          const dateQuery = filter.query as DateQuery;
+          str += `${filter.type} ${equality} ${datePipe.transform(dateQuery.date)}`;
+          break;
+        case 'state':
+          const stateQuery = filter.query as StateQuery;
+          str += `${filter.type} ${equality} ${stateQuery.state}`;
+          break;
+      }
+    });
+
+    return str;
+  }));
 }
