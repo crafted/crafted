dbf73c51acb08a81034bfad521b4d8b01dc33982
Andrew Seguin <andrewjs@google.com>
2019-04-24 10:00:50 -0700

    initial recommendation json validation

--- projects/github-dashboard/src/app/repository/shared/dialog/recommendation/recommendations-edit-json/recommendations-edit-json.module.ts
+++ projects/github-dashboard/src/app/repository/shared/dialog/recommendation/recommendations-edit-json/recommendations-edit-json.module.ts
@@ -2,11 +2,24 @@ import {TextFieldModule} from '@angular/cdk/text-field';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
-import {MatButtonModule, MatDialogModule} from '@angular/material';
+import {
+  MatButtonModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatIconModule
+} from '@angular/material';
 import {RecommendationsEditJson} from './recommendations-edit-json';
 
 @NgModule({
-  imports: [CommonModule, MatDialogModule, MatButtonModule, ReactiveFormsModule, TextFieldModule],
+  imports: [
+    CommonModule,
+    MatDialogModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    TextFieldModule,
+    MatExpansionModule,
+    MatIconModule,
+  ],
   declarations: [RecommendationsEditJson],
   exports: [RecommendationsEditJson],
   entryComponents: [RecommendationsEditJson]
--- projects/github-dashboard/src/app/repository/shared/dialog/recommendation/recommendations-edit-json/recommendations-edit-json.ts
+++ projects/github-dashboard/src/app/repository/shared/dialog/recommendation/recommendations-edit-json/recommendations-edit-json.ts
@@ -22,6 +22,9 @@ export interface RecommendationsEditJsonResult {
 export class RecommendationsEditJson {
   jsonForm = new FormControl('[]', recommendationsValidator);
 
+  errors = Errors;
+
+  errorCount = () => Object.keys(this.jsonForm.errors).length;
 
   private destroyed = new Subject();
 
@@ -50,18 +53,99 @@ export class RecommendationsEditJson {
 }
 
 function recommendationsValidator(control: AbstractControl): {[key: string]: any}|null {
+  const validation: {[key in string]: {recommendation: Recommendation|null}} = {};
   let recommendations: Recommendation[];
 
   try {
     recommendations = JSON.parse(control.value) as Recommendation[];
   } catch {
-    return {'invalid': {value: control.value}};
+    validation['invalidJson'] = {recommendation: null};
+    return validation;
   }
 
-  const valid = recommendations.every(recommendation => {
-    return !!recommendation.action && !!recommendation.actionType && !!recommendation.data &&
-        !!recommendation.filtererState && !!recommendation.message && !!recommendation.type;
+  const ids = new Set();
+  recommendations.forEach(recommendation => {
+    if (recommendation.id && ids.has(recommendation.id)) {
+      validation['duplicateId'] = {recommendation};
+    }
+
+    if (!recommendation.id) {
+      validation['missingId'] = {recommendation};
+    }
+
+    ids.add(recommendation.id);
+
+    if (!recommendation.action) {
+      validation['missingAction'] = {recommendation};
+    }
+
+    if (!recommendation.actionType) {
+      validation['missingActionType'] = {recommendation};
+    }
+
+    if (!recommendation.data) {
+      validation['missingData'] = {recommendation};
+    }
+
+    if (!recommendation.filtererState) {
+      validation['missingFiltererState'] = {recommendation};
+    }
+
+    if (!recommendation.message) {
+      validation['missingMessage'] = {recommendation};
+    }
+
+    if (!recommendation.type) {
+      validation['missingType'] = {recommendation};
+    }
   });
 
-  return valid ? null : {'invalid': {value: control.value}};
+  return Object.keys(validation).length ? validation : null;
+}
+
+type ValidationError = 'invalidJson'|'duplicateId'|'missingId'|'missingAction'|'missingActionType'|
+    'missingData'|'missingFiltererState'|'missingMessage'|'missingType';
+
+
+const Errors: {id: ValidationError, message: (r: Recommendation) => string}[] = [
+  {
+    id: 'invalidJson',
+    message: () => 'JSON is invalid',
+  },
+  {
+    id: 'duplicateId',
+    message: r => `Cannot have two recommendations with the same ID (${r.id})`,
+  },
+  {
+    id: 'missingId',
+    message: r => `Missing ID for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingAction',
+    message: r => `Missing action for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingActionType',
+    message: r => `Missing action type for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingData',
+    message: r => `Missing data type for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingFiltererState',
+    message: r => `Missing filterer state for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingMessage',
+    message: r => `Missing message for recommendation: "${recToString(r)}"`,
+  },
+  {
+    id: 'missingType',
+    message: r => `Missing type for recommendation: "${recToString(r)}"`,
+  },
+];
+
+function recToString(r: Recommendation) {
+  return r.message ? r.message : r.id ? r.id : JSON.stringify(r);
 }
