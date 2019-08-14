5fdde2f45a19edeb7e1132d740e18aafc77695b2
andrewseguin <andrewjs@google.com>
2019-08-14 08:27:49 -0700

    docs: fix dashboard demo

--- projects/docs/src/app/demo-dashboard/demo-dashboard.ts
+++ projects/docs/src/app/demo-dashboard/demo-dashboard.ts
@@ -6,7 +6,17 @@ import {
   getListWidgetConfig,
   WidgetConfig
 } from '@crafted/components';
-import {DataResources, DataSource, Filterer, Grouper, Sorter, Viewer} from '@crafted/data';
+import {
+  DataSource,
+  Filterer,
+  FiltererState,
+  Grouper,
+  GrouperState,
+  Sorter,
+  SorterState,
+  Viewer,
+  ViewerState
+} from '@crafted/data';
 import {EXAMPLE_ITEMS} from '../data';
 import {EXAMPLE_DATA_SOURCE_METADATA} from '../data-resources/data-source-metadata';
 import {EXAMPLE_FILTERER_METADATA} from '../data-resources/filterer-metadata';
@@ -15,6 +25,15 @@ import {EXAMPLE_SORTER_METADATA} from '../data-resources/sorter-metadata';
 import {EXAMPLE_VIEWER_METADATA} from '../data-resources/viewer-metadata';
 import {getBarChartWidgetConfig} from './bar-chart/bar-chart';
 
+export interface DataResources {
+  type: string;
+  label: string;
+  viewer: (initialState?: ViewerState) => Viewer;
+  filterer: (initialState?: FiltererState) => Filterer;
+  grouper: (initialState?: GrouperState) => Grouper;
+  sorter: (initialState?: SorterState) => Sorter;
+  dataSource: () => DataSource;
+}
 
 @Component({
   selector: 'demo-dashboard',
@@ -25,6 +44,7 @@ export class DemoDashboard {
   edit = true;
 
   dashboard: Dashboard = {
+    id: 'my-dashboard',
     name: 'New Dashboard',
     columnGroups: [{
       columns: [
@@ -37,15 +57,15 @@ export class DemoDashboard {
 
   dataResourcesMap = new Map<string, DataResources>([
     [
-      'issue', {
-        type: 'issue',
-        label: 'Issues',
+      'item', {
+        type: 'item',
+        label: 'Items',
         dataSource: () =>
-          new DataSource({data: EXAMPLE_ITEMS, metadata: EXAMPLE_DATA_SOURCE_METADATA}),
-      viewer: initialState => new Viewer({metadata: EXAMPLE_VIEWER_METADATA, initialState}),
-      filterer: initialState => new Filterer({metadata: EXAMPLE_FILTERER_METADATA, initialState}),
-      grouper: initialState => new Grouper({metadata: EXAMPLE_GROUPER_METADATA, initialState}),
-      sorter: initialState => new Sorter({metadata: EXAMPLE_SORTER_METADATA, initialState}),
+            new DataSource({data: EXAMPLE_ITEMS, metadata: EXAMPLE_DATA_SOURCE_METADATA}),
+        viewer: initialState => new Viewer({metadata: EXAMPLE_VIEWER_METADATA, initialState}),
+        filterer: initialState => new Filterer({metadata: EXAMPLE_FILTERER_METADATA, initialState}),
+        grouper: initialState => new Grouper({metadata: EXAMPLE_GROUPER_METADATA, initialState}),
+        sorter: initialState => new Sorter({metadata: EXAMPLE_SORTER_METADATA, initialState}),
       }
     ],
   ]);
